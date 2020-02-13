9f2eebb171450340256234a34dcfa216fd4327f4
Kyle Husmann <kyle.husmann@gmail.com>
2020-02-13 16:03:31 -0500

    fixed build failure: unset rogue CXXFLAGS option
    Resolves conda-forge/r-openmx-feedstock#16. Build fails
    because OpenMx uses gnu++14, but conda sets DEBUG_CXXFLAGS
    to use c++17, which overwrites OpenMx's choice. But OpenMx
    expects DEBUG_CXXFLAGS to only be set when enabling debug
    options, so the best fix seems to be to just unset it here
    before compiling. Also see: conda/conda-build#3097

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]] || [[ $target_platform == win-64 ]] || [[ $target_platform == osx-64 ]]; then
   export DISABLE_AUTOBREW=1
+  
+  # The OpenMx uses this var to toggle debug behavior within
+  # its own build system. Conda sets this var also, causing
+  # the OpenMx build to crash. Better unset it here so
+  # OpenMx can use its own DEBUG_CXXFLAGS
+  unset DEBUG_CXXFLAGS
+  
   $R CMD INSTALL --build .
 else
   mkdir -p $PREFIX/lib/R/library/OpenMx
